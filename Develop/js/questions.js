6c9db55d25f69ef2da4d2a29152d2f0fcbe7e094
damquixote <bleusd3@gmail.com>
2022-07-07 23:12:28 -0400

    first rendition

---
+++ Develop/js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types do NOT include?",
+    answer: "3. alerts",
+    options: [
+      "1. strings",
+      "2. booleans",
+      "3. alerts",
+      "4. numbers"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed with _____.",
+    answer: "3. parenthesis",
+    options: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parenthesis",
+      "4. square brackets"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Arrays in javascript can be used to store ____.",
+    answer: "4. all of the above",
+    options: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    answer: "3. quotes",
+    options: [
+      "1. commas",
+      "2. curly brackets",
+      "3. quotes",
+      "4. parenthesis"
+    ]
+  },
+    {
+    numb: 5,
+    question: "A very useful tool used during devleopment and debugging for printing content to the debugger is:",
+    answer: "4. console.log",
+    options: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loop",
+      "4. console.log"
+    ]
+  },
+];
